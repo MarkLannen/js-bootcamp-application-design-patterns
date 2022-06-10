47b6b0f19b145724ee55bf8a04c04158e286ffd8
MarkLannen <marklannen@gmail.com>
2022-06-09 20:55:55 -0600

    add search input field

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '65d7eca0',
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+  fetchData(event.target.value);
+});
