5fd099ccc3def799d637d791b76a9532921316b9
Mark Lannen <mark.lannen@mso.umt.edu>
2022-06-15 11:41:37 -0600

    add debounce function

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId)
+const onInput =  async (event) => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}"/>
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
   }
-  timeoutId = setTimeout(()=> {
-    fetchData(event.target.value);
-  }, 1000)
 };
 
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput, 500));
